3ccffb0a4e2ba1c02129b1eb3d6d69b29476080e
Terence <zunmun@gmail.com>
2024-01-20 16:56:55 +0800

    feat: file upload and save

--- backend/optical/optical/views.py
+++ backend/optical/optical/views.py
@@ -1,6 +1,23 @@
 from django.http import JsonResponse
-
-
+import os
 def my_view(request):
+    if request.method == 'POST':
+        if request.FILES:
+            print(request.FILES)
+            uploaded_file = request.FILES['file']
+            print("Uploaded File:", uploaded_file.name)
+
+            # Read the content from the UploadedFile
+            file_content = uploaded_file.read()
+            # Specify the path where you want to save the file
+            save_path = os.path.join('save.png')
+            # Save the content to the new file
+            with open(save_path, 'wb') as destination_file:
+                destination_file.write(file_content)
+
+        # Handle the form data here
+        form_data = request.POST
+        return JsonResponse({"message": f'Form data received: {form_data}'})
+        
     data = {"message": "Hello, Django!"}
     return JsonResponse(data)
