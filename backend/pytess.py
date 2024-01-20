5072e7727176ae6071bed085ace3acb2aeb63de7
John Wee <91670254+johnwee1@users.noreply.github.com>
2024-01-20 17:06:14 +0800

    add new samples

--- backend/pytess.py
+++ backend/pytess.py
@@ -1,11 +1,32 @@
 from tesserocr import PyTessBaseAPI
+from PIL import Image
 
-images = ["grace.jpg", "joseph1.jpg"]
+image = "sample_4.jpeg"
+
+
+def preprocess_to_grayscale(input_image_path):
+    # changes images to grayscale
+    # Open the image file
+    original_image = Image.open(input_image_path)
+
+    # Convert the image to grayscale
+    grayscale_image = original_image.convert("L")
+
+    # Save the preprocessed image
+    grayscale_image.save(input_image_path)
+
+
+preprocess_to_grayscale(image)
+
+images = []
+images.append(image)
 
 with PyTessBaseAPI() as api:
     for img in images:
         api.SetImageFile(img)
-        print(api.GetUTF8Text())
+        text = api.GetUTF8Text()
+        print(text)
+        print(len(text))
         print(api.AllWordConfidences())
 # api is automatically finalized when used in a with-statement (context manager).
 # otherwise api.End() should be explicitly called when it's no longer needed.
