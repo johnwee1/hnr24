059136f775dd2c4e6e5ab78df3365c635c3c64f6
Hackin7 <zunmun@gmail.com>
2024-01-20 16:13:02 +0800

    feat: camera image upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,22 +5,79 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import {useState} from 'react';
+
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
+
+const ENDPOINT = "http://localhost:5555";
+window.state = {};
+
+function CameraUse (props) {
+  const [info, setInfo] = useState(false) ;
+  function handleTakePhoto (dataUri) {
+    // Do stuff with the photo...
+    console.log('takePhoto');
+    alert(dataUri);
+    fetch(dataUri)
+    .then(response => response.blob())
+    .then(blob => {
+        const file = new File([blob], "sample.png", {type: blob.type})
+        window.state.file = file;
+        console.log(file);    //File object
+        alert(file);
+        return file
+    }).then(file => {
+      const formData = new FormData();
+        formData.append("file",file);
+        const fetchOptions = {
+          method: "POST",
+          body: formData,
+        };
+        
+        fetch(ENDPOINT, fetchOptions)
+        .then(response => response.json())
+        .then((data) => {
+          setInfo(data);
+        })
+        .catch((err)=>{
+          alert(err);
+          setInfo({"test":1});
+        });
+    })
+  }
+
+  const text = "the quick brown fox jump over the lazy dog";
+  return (
+    <>
+      {
+        info === false ? 
+        <>
+          <h1>Take a photo of handwriting!</h1>
+          <p>Text to write: "{text}"</p>
+          <Camera
+            onTakePhoto = { (dataUri) => { handleTakePhoto(dataUri); } }
+          />
+        </>
+        :
+        <>
+          {JSON.stringify(info)}
+        </>
+      }
+    </>
+  );
+}
+
 
 function App() {
   return (
     <Router>
         <Routes>
             <Route exact path="/" element={<h1>Home</h1>} />
-            <Route path="/about" element={<h1>about</h1>} />
-            <Route
-                path="/contact"
-                element={<h1>Home</h1>}
-            />
-            <Route path="/blogs" element={<h1>blogs</h1>} />
-            <Route
-                path="/sign-up"
-                element={<h1>Home</h1>}
-            />
+            <Route path="/about" element={<CameraUse/>} />
+            <Route path="/contact" element={<h1>Home</h1>}/>
+            <Route path="/blogs" element={<h1>blogs</h1>}/>
+            <Route path="/error" element={<h1>Error</h1>}/>
         </Routes>
     </Router>
   );
