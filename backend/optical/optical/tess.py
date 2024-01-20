f6184fc5c2b55601fb9358dcc02b3cbf134cb4da
John Wee <91670254+johnwee1@users.noreply.github.com>
2024-01-21 02:39:27 +0800

    cooked ocr boys

---
+++ backend/optical/optical/tess.py
@@ -0,0 +1,30 @@
+from tesserocr import PyTessBaseAPI
+import os
+import optical.preprocessing as pp
+import optical.chatgpt as chatgpt
+import numpy as np
+import cv2
+from PIL import Image
+
+
+# Get image
+def tesseract(img):
+    nd_thresh = pp.preprocessing_cv2(img)
+    img_thresh = Image.fromarray(nd_thresh.astype("uint8"))
+    text = None
+    filedir = os.path.dirname(__file__)
+    filedir = os.path.join(filedir, "tessdata_best")
+    with PyTessBaseAPI(path=filedir) as api:
+        api.SetImage(img_thresh)
+        text = api.GetUTF8Text()
+        print(text)
+        print(f"len={len(text)}")
+        print(api.AllWordConfidences())
+    if len(text) == 0:
+        return "Sentence not found"
+    # return chatgpt.prompt_model("prompt.txt", text)
+    return text
+
+
+# api is automatically finalized when used in a with-statement (context manager).
+# otherwise api.End() should be explicitly called when it's no longer needed.
